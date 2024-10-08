01b6ffada706c219d82f472ce23bd161e61d7431
BenderPlex <bvalverde01@manhattan.edu>
2024-10-08 14:59:59 -0400

    The code now outputs 2 graphs one showing temp vs O/F Ratio and the other shows temp vs chamber pressure

--- RocketCEA-code/Tcomb.py
+++ RocketCEA-code/Tcomb.py
@@ -1,26 +1,28 @@
 from rocketcea.cea_obj_w_units import CEA_Obj
-from pylab import *
+import matplotlib.pyplot as plt
 
 Pc = 0
 MR = 0
+PArr = []
 MRArr = []
 TArr = []
+f, (ax1, ax2) = plt.subplots(1,2 , sharey=True)
+ax1.set(xlabel='O/F Ratio', ylabel='Temperature [K]')
+ax2.set(xlabel='Chamber Pressure')
+ax1.grid()
+ax2.grid()
 
 TObj = CEA_Obj(oxName='LOX', fuelName='LH2', temperature_units='K', pressure_units='MPa' )
 while True:
     MR += 0.01
     Pc += 1/145
     MRArr.append(MR)
+    PArr.append(Pc)
     T = TObj.get_Tcomb(Pc, MR)
     TArr.append(T)
-    plot(MRArr, TArr)
+    ax1.plot(MRArr, TArr)
+    ax2.plot(PArr, TArr)
     if MR >= 15:
         break
 
-legend(loc='best')
-grid(True)
-title('Combustion Temperature vs. O/F Ratio')
-xlabel('O/F Ratio')
-ylabel('Temperature [K]')
-
-show()
+plt.show()
