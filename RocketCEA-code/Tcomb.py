646811e9b9cf2b4be3025f3e0ad5054b4deee4f3
Bender3 <bvalverde01@manhattan.edu>
2024-10-13 20:48:10 -0400

    This code now prints combustion temperature vs OF ratio at different chamber pressures

--- RocketCEA-code/Tcomb.py
+++ RocketCEA-code/Tcomb.py
@@ -1,28 +1,32 @@
 from rocketcea.cea_obj_w_units import CEA_Obj
 import matplotlib.pyplot as plt
+import numpy as np
 
-Pc = 0
-MR = 0
-PArr = []
-MRArr = []
-TArr = []
-f, (ax1, ax2) = plt.subplots(1,2 , sharey=True)
-ax1.set(xlabel='O/F Ratio', ylabel='Temperature [K]')
-ax2.set(xlabel='Chamber Pressure')
-ax1.grid()
-ax2.grid()
+figure, ax = plt.subplots()
+ax.set(ylabel='Temperature [K]')
+ax.set(xlabel='O/F Ratio')
+ax.set_title('Combustion Temperature vs O/F Ratio at different Chamber Pressures')
+ax.grid()
 
 TObj = CEA_Obj(oxName='LOX', fuelName='LH2', temperature_units='K', pressure_units='MPa' )
-while True:
-    MR += 0.01
-    Pc += 1/145
-    MRArr.append(MR)
-    PArr.append(Pc)
-    T = TObj.get_Tcomb(Pc, MR)
-    TArr.append(T)
-    ax1.plot(MRArr, TArr)
-    ax2.plot(PArr, TArr)
-    if MR >= 15:
-        break
+
+
+for P in [0.101325, 1, 2, 3, 4, 5, 6, 7 , 8, 9, 10]:
+    MR = 0 
+    MRArr = []
+    TArr = []
+    
+     # Reset MR for each new value of P
+    while MR <= 15:
+        MRArr.append(MR)
+        T = TObj.get_Tcomb(P, MR)
+        TArr.append(T)
+        print(P, MR, T)
+        MR += 0.5
+    
+
+    ax.plot(MRArr, TArr, label=f'{P} MPa')
+
+ax.legend()
 
 plt.show()
